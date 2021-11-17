dee308004ed2948e074d54a91202478f5912fc8e
annavandehey <apv282@nyu.edu>
2021-11-17 11:28:40 -0500

    button

---
+++ week11/thebutton.js
@@ -0,0 +1,35 @@
+console.log("theMessage is here! (the javascript is here)")
+
+const theButton = document.getElementById("firstButton");
+let theBody = document.querySelector("body");
+let thePara = document.querySelector("p");
+// console.log(thePara)
+
+var clicks = 0;
+
+function onClick() {
+  clicks += 1;
+  document.getElementById("clicks").innerHTML = clicks;
+};
+
+function makeDo(){
+    console.log("You clicked the button!");
+    theBody.style.backgroundColor = "green";
+    theBody.style.color = "white"
+    thePara.textContent = "you presesed the button once!"
+   
+}
+function makeDo2(){
+    console.log("You clicked the button!");
+    theBody.style.backgroundColor = "green";
+    theBody.style.color = "white"
+    thePara.textContent = "you presesed the button twice!"
+   
+}
+function makeDo3(){
+    console.log("You clicked the button!");
+    theBody.style.backgroundColor = "green";
+    theBody.style.color = "white"
+    thePara.textContent = "you presesed the button thrice!"
+   
+}
