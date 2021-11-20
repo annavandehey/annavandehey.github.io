6b10af915b14d9b15e793ae844fe8e03839d0212
annavandehey <apv282@nyu.edu>
2021-11-20 14:54:55 -0500

    themedium

--- week12/script.js
+++ week12/script.js
@@ -1,36 +1,20 @@
-console.log("theMessage is here! (the javascript is here)")
-
-var d = new Date();
-      var n = d.getHours();
-      if (n > 19 || n < 6)
-        $("#body").css("background", "url('references/sleep.gif')");
-      else if (n > 8 && n < 16)
-        $("#body").css("background", "url('references/geo.gif')");
-      else
-        $("#body").css("background", "url('references/free.gif')");
-
-
-jQuery(document).ready(function( $ ){
-
-    var thehours = new Date().getHours();
-        var themessage;
-        var school = ('have fun at school');
-        var sleep = ('enjoy your rest');
-        var free = ('enjoy your freetime');
-            
-                if (thehours >= 19 && thehours < 6) {
-                    themessage = sleep;
-                $("#greeting").addClass("sleep");
-            
-                } else if (thehours >= 8 && thehours < 16) {
-                    themessage = school;
-                $("#greeting").addClass("school");
-            
-                } else {
-                    themessage = free;
-                $("#greeting").addClass("free");
-                }
-            
-                $('#greeting').append(themessage);
-              
-            });
+if (typeof window === 'object') {
+    // Check if document is finally loaded
+       document.addEventListener("DOMContentLoaded", function () {
+           alert('Finished loading')
+         });
+      }
+      
+$(document).ready(function(){
+	var d = new Date();
+	var n = d.getHours();
+	if (n > 19 || n < 6)
+	  // If time is after 7PM or before 6AM, apply night theme to ‘body’
+	  document.body.className = "night";
+	else if (n > 16 && n < 19)
+	  // If time is between 4PM – 7PM sunset theme to ‘body’
+	  document.body.className = "sunset";
+	else
+	  // Else use ‘day’ theme
+	  document.body.className = "day";
+});
