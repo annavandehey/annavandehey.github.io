eefbe86d6e3d0c86b3587a46cfe62b3169e840a7
annavandehey <apv282@nyu.edu>
2021-12-12 23:53:41 -0500

    finaldraft2

---
+++ final/fowlbig.js
@@ -0,0 +1,24 @@
+console.log("The BIGjs");
+let donepicture = 2;
+
+// Elements on display on superlarge.html
+if ((donepicture = 2)) {
+    var element = document.getElementById("picture");
+	element.parentNode.removeChild(element);
+}
+if ((donephone = 2)) {
+    var newobj2 = document.getElementById("phone");
+    newobj2.remove();
+}
+if ((donepurse = 2)) {
+    var newobj3 = document.getElementById("purse");
+    newobj3.remove();
+}
+if ((donepurse = 2)) {
+    var newobj4 = document.getElementById("meals");
+    newobj4.remove();
+}
+if ((donepurse = 2)) {
+    var newobj5 = document.getElementById("window");
+    newobj5.remove();
+}
